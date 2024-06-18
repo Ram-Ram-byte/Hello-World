f4ee6aa609b96eccb77bbeaca187c78415982f40
DELL <DELL@DESKTOP-J8BOLV2>
2024-06-18 22:10:26 +0530

    added class Test.java

---
+++ Hello World/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int length=20;
+		System.out.println(a);
+	for(int i=0;i<20;i++) {
+		System.out.println(b);
+		int c= a+b;
+		a=b;
+		b=c;
+		
+	}
+	}
+
+}
